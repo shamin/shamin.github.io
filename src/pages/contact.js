90b91f91b0473a39ecc2009f14f1af592ba8c09a
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-08-10 19:51:51 +0530

    Added about and contact page and their corresponding style files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const SecondPage = () => (
+const Contact = () => (
   <div>
-    <h1>Hi from the second page</h1>
+    <h1>Contact Page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
 
-export default SecondPage
+export default Contact
