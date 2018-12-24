83b37d384eb1e0bb26e4446d75ba8f58e2d66bc8
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-12-24 10:33:49 +0530

    Fixed social links

--- src/components/social.js
+++ src/components/social.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 export default class Social extends React.Component {
   render() {
@@ -6,18 +6,42 @@ export default class Social extends React.Component {
       <div className="row">
         <div className="col-12">
           <p className="center">
-            <a href="https://twitter.com/shaminm07" className="icon"><i className="fab fa-twitter"></i></a>
-            <a href="https://www.linkedin.com/in/shamin-meerankutty" className="icon"><i className="fab fa-linkedin-in"></i></a>
-            <a href="https://github.com/shaminmeerankutty" className="icon"><i className="fab fa-github"></i></a>
-            <a href="https://www.behance.net/shamin_meerankutty" className="icon"><i className="fab fa-behance"></i></a>
-            <a href="https://www.instagram.com/shamin_meerankutty" className="icon"><i className="fab fa-instagram"></i></a>
-            <a href="https://plus.google.com/u/0/+ShaminMeerankutty" className="icon"><i className="fab fa-google-plus-g"></i></a>
-            <a href="https://medium.com/@shamin616" className="icon"><i className="fab fa-medium-m"></i></a>
-            <a href="https://codepen.io/shamin616" className="icon"><i className="fab fa-codepen"></i></a>
-            <a href="https://dribbble.com/shamin-meerankutty" className="icon"><i className="fab fa-dribbble"></i></a>
+            <a href="https://github.com/shaminmeerankutty" className="icon">
+              <i className="fab fa-github" />
+            </a>
+            <a
+              href="https://stackoverflow.com/users/4854945/shamin-meerankutty?tab=profile"
+              className="icon"
+            >
+              <i className="fab fa-stack-overflow" />
+            </a>
+            <a href="https://medium.com/@shamin616" className="icon">
+              <i className="fab fa-medium-m" />
+            </a>
+            <a href="https://codepen.io/shamin616" className="icon">
+              <i className="fab fa-codepen" />
+            </a>
+            <a href="https://dribbble.com/shamin-meerankutty" className="icon">
+              <i className="fab fa-dribbble" />
+            </a>
+            <a
+              href="https://www.behance.net/shamin_meerankutty"
+              className="icon"
+            >
+              <i className="fab fa-behance" />
+            </a>
+            <a href="https://twitter.com/shaminm07" className="icon">
+              <i className="fab fa-twitter" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/shamin-meerankutty"
+              className="icon"
+            >
+              <i className="fab fa-linkedin-in" />
+            </a>
           </p>
         </div>
       </div>
-    );
+    )
   }
 }
