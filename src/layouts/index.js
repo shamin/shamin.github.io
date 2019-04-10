37abceffed810dba3163fcf9a86479fb1175b0b0
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-04-11 00:09:09 +0530

    Removed all the unwanted files

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-// import Header from '../components/header'
-import Nav from '../components/nav'
 import '../styles/index.scss'
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
-      title={data.site.siteMetadata.title}
+      title="Shamin.me"
       meta={[
         { name: 'description', content: 'Shamin Meerankutty\'s Blog' },
         { name: 'keywords', content: 'blog, shamin, shamin meerankutty, shamin.me' },
@@ -17,8 +15,6 @@ const Layout = ({ children, data }) => (
     >
       <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
     </Helmet>
-    {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-    {/* <Nav /> */}
     <div>
       {children()}
     </div>
