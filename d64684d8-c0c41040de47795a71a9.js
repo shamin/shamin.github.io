7c6ca71993f6cce9d45377ddd2e431291c7cca8c
shamin.meerankutty <shamin616@gmail.com>
2021-06-05 18:18:21 +0530

    Updates

--- d64684d8-c0c41040de47795a71a9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{PRV2:function(n,t,o){"use strict";o.d(t,"a",(function(){return a}));var r=o("Lnxd");function a(n){return Object(r.a)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"}}]})(n)}}}]);
-//# sourceMappingURL=d64684d8-c0c41040de47795a71a9.js.map
